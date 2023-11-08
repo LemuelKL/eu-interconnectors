8f8dc99ddadff2c23458428d0eb784c136f56fd0
Lemuel Lee <lemuellee.kl@gmail.com>
2023-11-09 01:25:52 +0800

    qDayAheadPrices

--- client.py
+++ client.py
@@ -39,7 +39,10 @@ class Client:
     def query_api(self, optionDict):
         url = self.get_request_url(optionDict)
         response = requests.get(url)
-        print(response.status_code)
+        if response.status_code != 200:
+            print("Error: " + str(response.status_code))
+            print(response.text)
+            exit(1)
         respText = response.text
         with open("response.xml", "w") as f:
             f.write(respText)
@@ -57,14 +60,7 @@ class Client:
     # 4.2.15. Physical Flows [12.1.G]
     # https://transparency.entsoe.eu/content/static_content/Static%20content/web%20api/Guide.html#:~:text=4.2.15.%20Physical%20Flows%20%5B12.1.G%5D
     def qPhysicalFlows(self, in_domain, out_domain, start, end):
-        # optionDict = {
-        #     "documentType": "A11",
-        #     "in_Domain": "10YGB----------A",
-        #     "out_Domain": "10YFR-RTE------C",
-        #     "periodStart": "201810310000",
-        #     "periodEnd": "201811070000",
-        # }
-        respDict = self.query_api(
+        resp_dict = self.query_api(
             {
                 "documentType": "A11",
                 "in_Domain": in_domain,
@@ -74,18 +70,20 @@ class Client:
             }
         )
 
-        timeseriesDatas = [] + [
-            respDict["Publication_MarketDocument"]["TimeSeries"]
-        ]  # this is a hack to explicitly create a list
+        TimeSeries = resp_dict["Publication_MarketDocument"]["TimeSeries"]
+        if isinstance(TimeSeries, list):
+            timeseries_datas = TimeSeries
+        else:
+            timeseries_datas = [TimeSeries]
 
         df = pd.DataFrame(columns=["quantity"])
-        for timeseriesData in timeseriesDatas:
-            time_interval_start = timeseriesData["Period"]["timeInterval"]["start"]
-            time_interval_end = timeseriesData["Period"]["timeInterval"]["end"]
-            resolution = timeseriesData["Period"]["resolution"]
+        for timeseries_data in timeseries_datas:
+            time_interval_start = timeseries_data["Period"]["timeInterval"]["start"]
+            time_interval_end = timeseries_data["Period"]["timeInterval"]["end"]
+            resolution = timeseries_data["Period"]["resolution"]
             assert resolution in RESOLUTION_TABLE
             print(time_interval_start, time_interval_end, resolution)
-            data_points = timeseriesData["Period"]["Point"]
+            data_points = timeseries_data["Period"]["Point"]
             time_counter = pd.date_range(
                 start=time_interval_start,
                 end=time_interval_end,
@@ -107,4 +105,54 @@ class Client:
                 )
 
         df["quantity"] = pd.to_numeric(df["quantity"])
+        return df
+
+    # 4.2.10. Day Ahead Prices [12.1.D]
+    # https://transparency.entsoe.eu/content/static_content/Static%20content/web%20api/Guide.html#:~:text=4.2.10.%20Day%20Ahead%20Prices%20%5B12.1.D%5D
+    def qDayAheadPrices(self, domain, start, end):
+        resp_dict = self.query_api(
+            {
+                "documentType": "A44",
+                "in_Domain": domain,
+                "out_Domain": domain,
+                "periodStart": start,
+                "periodEnd": end,
+            }
+        )
+        TimeSeries = resp_dict["Publication_MarketDocument"]["TimeSeries"]
+        if isinstance(TimeSeries, list):
+            timeseries_datas = TimeSeries
+        else:
+            timeseries_datas = [TimeSeries]
+
+        df = pd.DataFrame(columns=["price"])
+        for timeseries_data in timeseries_datas:
+            period = timeseries_data["Period"]
+            time_interval_start = period["timeInterval"]["start"]
+            time_interval_end = period["timeInterval"]["end"]
+            resolution = period["resolution"]
+            assert resolution in RESOLUTION_TABLE
+            print(time_interval_start, time_interval_end, resolution)
+            data_points = period["Point"]
+            time_counter = pd.date_range(
+                start=time_interval_start,
+                end=time_interval_end,
+                freq=RESOLUTION_TABLE[resolution],
+            )
+            for data_point in data_points:
+                df = pd.concat(
+                    [
+                        df,
+                        pd.DataFrame(
+                            {
+                                "price": data_point["price.amount"],
+                            },
+                            index=[
+                                time_counter[int(data_point["position"]) - 1]
+                            ],  # position starts at 1
+                        ),
+                    ],
+                )
+
+        df["price"] = pd.to_numeric(df["price"])
         return df
--- main.py
+++ main.py
@@ -2,6 +2,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from client import Client
 
+# BZN — Bidding Zone
+# BZA — Bidding Zone Aggregation
+# CTA — Control Area
+# MBA — Market Balance Area
+# IBA — Imbalance Area
+# IPA — Imbalance Price Area
+# LFA — Load Frequency Control Area
+# LFB — Load Frequency Control Block
+# REG — Region
+# SCA — Scheduling Area
+# SNA — Synchronous Area
+
+# 10YGB----------A
+# LFA|GB, LFB|GB, SNA|GB, MBA|GB, SCA|GB, CTA|National Grid, BZN|GB
+
+# 10YFR-RTE------C
+# BZN|FR, France (FR), CTA|FR, SCA|FR, MBA|FR, LFB|FR, LFA|FR
+
+# 10YNL----------L
+# LFA|NL, LFB|NL, CTA|NL, Netherlands (NL), BZN|NL, SCA|NL, MBA|NL
+
 client = Client()
 df = client.qPhysicalFlows(
     in_domain="10YGB----------A",
@@ -9,5 +30,10 @@ df = client.qPhysicalFlows(
     start="201810310000",
     end="201811070000",
 )
-
-print(df.head())
+df.to_csv("data_physical_flows.csv")
+df = client.qDayAheadPrices(
+    domain="10YNL----------L",
+    start="201810310000",
+    end="201811070000",
+)
+df.to_csv("data_day_ahead_prices.csv")
