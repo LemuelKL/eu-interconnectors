a049a0b491ae619394a3a3a5a9af6ef815d2eb98
Lemuel Lee <lemuellee.kl@gmail.com>
2023-11-20 02:45:46 +0800

    query everything

--- main.py
+++ main.py
@@ -50,42 +50,42 @@ class Interconnector:
         return f"{self.country} {self.in_domain} {self.out_domain}"
 
 
+# Settings
+global_start = "202310010000"
+global_end = "202311010000"
+
 # Read interconnectors from file
-# interconnectors = pd.read_csv("config_physical_flow_cty.csv")
-# print(interconnectors)
+interconnectors = pd.read_csv("config_physical_flow_cty.csv")
+print(interconnectors)
 
 # Get physical flows for each interconnector
-# for idx, interconnector in interconnectors.iterrows():
-#     settings = {
-#         "in_domain": interconnector.in_domain,
-#         "out_domain": interconnector.out_domain,
-#         "start": "202111010000",
-#         "end": "202311010000",
-#     }
-#     df = client.qPhysicalFlows(**settings)
-#     df.to_csv(
-#         f"{DATA_DIR}/physical_flow__{interconnector.country}_{settings['in_domain']}_{settings['out_domain']}_{settings['start']}_{settings['end']}.csv"
-#     )
+for idx, interconnector in interconnectors.iterrows():
+    settings = {
+        "in_domain": interconnector.in_domain,
+        "out_domain": interconnector.out_domain,
+        "start": global_start,
+        "end": global_end,
+    }
+    df = client.qPhysicalFlows(**settings)
+    print(df)
+    df.to_csv(
+        f"{DATA_DIR}/physical_flow__{interconnector.country}_{settings['in_domain']}_{settings['out_domain']}_{settings['start']}_{settings['end']}.csv"
+    )
 
-# df = client.qDayAheadPrices(
-#     domain="10YNL----------L",
-#     start="202301010000",
-#     end="202310010000",
-# )
-# df.to_csv("data_day_ahead_prices.csv")
-# # Plot
-# df["day_moving_average"] = df["price"].rolling(window=24).mean()
-# df.plot()
-# plt.ylabel("Price (€/MWh)")
-# plt.xlabel("Time")
-# plt.title("Day Ahead Prices of Netherlands (NL)")
-# plt.show()
-
-
-df = client.qActualGenerationPerProductionType(
+df = client.qDayAheadPrices(
     domain="10YNL----------L",
-    start="202310010000",
-    end="202311010000",
+    start=global_start,
+    end=global_end,
 )
 print(df)
-df.to_csv(f"{DATA_DIR}/actual_generation_per_production_type.csv")
+df.to_csv(
+    f"{DATA_DIR}/day_ahead_price__10YNL----------L_{global_start}_{global_end}.csv"
+)
+
+df = client.qActualGenerationPerProductionType(
+    domain="10YNL----------L", start=global_start, end=global_end
+)
+print(df)
+df.to_csv(
+    f"{DATA_DIR}/actual_generation_per_production_type__10YNL----------L_{global_start}_{global_end}.csv"
+)
